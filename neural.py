3b8ec71e1a4981ff319fa92e337841f75d2e7ef1
Quentin SANTOS <quentin.santos@worldonline.fr>
2015-02-12 18:08:44 +0100

    Use indexed pool of neurons

--- neural.py
+++ neural.py
@@ -14,40 +14,46 @@ def sigmoid_prime(x):
 class DummyUnit:
     output = 1.
     grad = 0.
-dummy_unit = DummyUnit()
+neurons = [DummyUnit()]
 
 
 class Neuron:
     def __init__(self, inputs=[]):
-        self.inputs = inputs + [dummy_unit]
+        self.inputs = inputs + [0]
         self.weights = [1.] * len(self.inputs)
         self.output = 0.
         self.grad = 0.
 
     def propagate(self):
         self.output = sigmoid(sum(
-            x.output*w for x, w in zip(self.inputs, self.weights)))
+            neurons[i].output*w for i, w in zip(self.inputs, self.weights)))
         self.grad = 0.
 
     def backpropagate(self):
-        self.weights = [
-            w + self.grad*x.output for x, w in zip(self.inputs, self.weights)]
+        self.weights = [w + self.grad*neurons[i].output
+                        for i, w in zip(self.inputs, self.weights)]
         grad = sigmoid_prime(self.grad)
-        for x in self.inputs:
+        for i in self.inputs:
+            x = neurons[i]
             x.grad += x.output * grad
 
-inputs = [Neuron(), Neuron()]
-neuron = Neuron(inputs)
+neurons.append(Neuron())
+neurons.append(Neuron())
+neurons.append(Neuron([1, 2]))
+
+neuron = neurons[3]
 
 f = (((0, 0), 0), ((0, 1), 1), ((1, 0), 1), ((1, 1), 1))
 data = (random.choice(f) for _ in range(500))
 for input_values, output in data:
-    inputs[0].output, inputs[1].output = input_values
+    for idx, value in enumerate(input_values):
+        neurons[idx+1].output = value
     neuron.propagate()
     neuron.grad = output - neuron.output
     neuron.backpropagate()
 
 for input_values, output in f:
-    inputs[0].output, inputs[1].output = input_values
+    for idx, value in enumerate(input_values):
+        neurons[idx+1].output = value
     neuron.propagate()
     print(output, neuron.output)
