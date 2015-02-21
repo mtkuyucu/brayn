2abc31d46f7b88923e3a77cf9e0571ef118b463f
Quentin SANTOS <quentin.santos@worldonline.fr>
2015-02-21 22:35:17 +0100

    Merge code to retrieve test and training data

--- digits.py
+++ digits.py
@@ -7,19 +7,9 @@ nn.add_layer(10)
 n_iterations = 1
 
 
-def data():
-    for _ in range(n_iterations):
-        labels = mnist.labels_from("mnist/train-labels-idx1-ubyte.gz")
-        images = mnist.images_from("mnist/train-images-idx3-ubyte.gz")
-        for label, image in zip(labels, images):
-            image = (pixel / 256. for pixel in image)
-            expect = [0.]*label + [1.] + [0.]*(10-label)
-            yield image, expect
-
-
-def tests():
-    labels = mnist.labels_from("mnist/t10k-labels-idx1-ubyte.gz")
-    images = mnist.images_from("mnist/t10k-images-idx3-ubyte.gz")
+def from_mnist(label_file, image_file):
+    labels = mnist.labels_from(label_file)
+    images = mnist.images_from(image_file)
     for label, image in zip(labels, images):
         image = (pixel / 256. for pixel in image)
         expect = [0.]*label + [1.] + [0.]*(10-label)
@@ -27,12 +17,17 @@ def tests():
 
 
 print("training")
-for inputs, expect in data():
-    nn.train(inputs, expect)
+for _ in range(n_iterations):
+    data = from_mnist("mnist/train-labels-idx1-ubyte.gz",
+                      "mnist/train-images-idx3-ubyte.gz")
+    for inputs, expect in data:
+        nn.train(inputs, expect)
 
 print("testing")
 classifications = [[0]*10 for _ in range(10)]
-for inputs, expect in tests():
+data = from_mnist("mnist/t10k-labels-idx1-ubyte.gz",
+                  "mnist/t10k-images-idx3-ubyte.gz")
+for inputs, expect in data:
     # run neural network
     output = nn.compute(inputs)
     # recover labels
